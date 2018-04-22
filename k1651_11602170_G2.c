b8cc7dec4c79ce302de1101d198d43e29dd5b007
Anshuman Singh <anshumansingh005@gmail.com>
2018-04-23 01:03:48 +0530

    Update k1651_11602170_G2.c

--- k1651_11602170_G2.c
+++ k1651_11602170_G2.c
@@ -17,24 +17,7 @@ scanf(" %d",&at[i]);
 
 /*Sorting According to Arrival Time*/
 
-for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-{
-if(at[i]<at[j])
-{
-temp=p[j];
-p[j]=p[i];
-p[i]=temp;
-temp=at[j];
-at[j]=at[i];
-at[i]=temp;
-temp=bt[j];
-bt[j]=bt[i];
-bt[i]=temp;
-}
-}
-}
+
 
 /*Arranging the table according to Burst time,
 Execution time and Arrival Time
