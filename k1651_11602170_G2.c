146deb1690d0c75004775a6484eaff87c1d1b8fe
Anshuman Singh <anshumansingh005@gmail.com>
2018-04-23 01:04:30 +0530

    Update k1651_11602170_G2.c

--- k1651_11602170_G2.c
+++ k1651_11602170_G2.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
-int bt[10],temp,j,at[10],wt[10],tt[10],ta=0,sum=0;
-float wavg=0,tavg=0,tsum=0,wsum=0;
-printf("\nEnter the No. of processes :");
-scanf("%d",&n);
+  int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
+  int bt[10],temp,j,at[10],wt[10],tt[10],ta=0,sum=0;
+  float wavg=0,tavg=0,tsum=0,wsum=0;
+  printf("\nEnter the No. of processes :");
+  scanf("%d",&n);
 
-for(i=0;i<n;i++)
-{
+  for(i=0;i<n;i++)
+    {
 printf("\tEnter the burst time of process no. %d  :",i+1);
 scanf(" %d",&bt[i]);
 printf("\tEnter the arrival time of process no. %d :",i+1);
@@ -17,7 +17,24 @@ scanf(" %d",&at[i]);
 
 /*Sorting According to Arrival Time*/
 
-
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(at[i]<at[j])
+{
+temp=p[j];
+p[j]=p[i];
+p[i]=temp;
+temp=at[j];
+at[j]=at[i];
+at[i]=temp;
+temp=bt[j];
+bt[j]=bt[i];
+bt[i]=temp;
+}
+}
+}
 
 /*Arranging the table according to Burst time,
 Execution time and Arrival Time
