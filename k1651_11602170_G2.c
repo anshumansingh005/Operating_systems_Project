524daa1a0f8cd4e4b5986c8a4bd9638c67033da9
Anshuman Singh <anshumansingh005@gmail.com>
2018-04-23 01:10:57 +0530

    Update k1651_11602170_G2.c

--- k1651_11602170_G2.c
+++ k1651_11602170_G2.c
@@ -9,89 +9,89 @@ int main()
 
   for(i=0;i<n;i++)
     {
-printf("\tEnter the burst time of process no. %d  :",i+1);
-scanf(" %d",&bt[i]);
-printf("\tEnter the arrival time of process no. %d :",i+1);
-scanf(" %d",&at[i]);
-}
+     printf("\tEnter the burst time of process no. %d  :",i+1);
+     scanf(" %d",&bt[i]);
+     printf("\tEnter the arrival time of process no. %d :",i+1);
+     scanf(" %d",&at[i]);
+    }
 
 /*Sorting According to Arrival Time*/
 
-for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-{
-if(at[i]<at[j])
-{
-temp=p[j];
-p[j]=p[i];
-p[i]=temp;
-temp=at[j];
-at[j]=at[i];
-at[i]=temp;
-temp=bt[j];
-bt[j]=bt[i];
-bt[i]=temp;
-}
-}
-}
+  for(i=0;i<n;i++)
+    {
+      for(j=0;j<n;j++)
+        {
+          if(at[i]<at[j])
+            {
+              temp=p[j];
+              p[j]=p[i];
+              p[i]=temp;
+              temp=at[j];
+              at[j]=at[i];
+              at[i]=temp;
+              temp=bt[j];
+              bt[j]=bt[i];
+              bt[i]=temp;           
+            }
+         }
+     }
 
 /*Arranging the table according to Burst time,
 Execution time and Arrival Time
 Arrival time <= Execution time
 */
 
-for(j=0;j<n;j++)
-{
-btime=btime+bt[j];
-min=bt[k];
-i=k;
-while(i<n)
-{
-if (btime>=at[i] && bt[i]<min)
-{
-temp=p[k];
-p[k]=p[i];
-p[i]=temp;
-temp=at[k];
-at[k]=at[i];
-at[i]=temp;
-temp=bt[k];
-bt[k]=bt[i];
-bt[i]=temp;
-}
-i++;
-}
-k++;
-}
-wt[0]=0;
-for(i=1;i<n;i++)
-{
-sum=sum+bt[i-1];
-wt[i]=sum-at[i];
-wsum=wsum+wt[i];
-}
+  for(j=0;j<n;j++)
+   {
+      btime=btime+bt[j];
+      min=bt[k];
+      i=k;
+      while(i<n)
+        {
+            if (btime>=at[i] && bt[i]<min)
+              {
+                temp=p[k];
+                p[k]=p[i];
+                p[i]=temp;
+                temp=at[k];
+                at[k]=at[i];
+                at[i]=temp;
+                temp=bt[k];
+                bt[k]=bt[i];
+                bt[i]=temp;
+              }
+            i++;
+        }
+      k++;
+    }
+  wt[0]=0;
+  for(i=1;i<n;i++)
+    { 
+      sum=sum+bt[i-1];
+      wt[i]=sum-at[i];  
+      wsum=wsum+wt[i];
+    }
 
-wavg=(wsum/n);
-i= 0;
-while(i<n)
-{
-ta=ta+bt[i];
-tt[i]=ta-at[i];
-tsum=tsum+tt[i];
-++i;
-}
+  wavg=(wsum/n);
+  i= 0;
+  while(i<n)
+    {
+      ta=ta+bt[i];
+      tt[i]=ta-at[i];
+      tsum=tsum+tt[i];
+      ++i;
+    }
 
-tavg=(tsum/n);
+  tavg=(tsum/n);
 
-printf("************************");
-printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
-for(i=0;i<n;i++)
-{
-printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],at[i],wt[i],tt[i]);
-}
+  printf("************************");
+  printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
+  for(i=0;i<n;i++)
+    {
+      printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],at[i],wt[i],tt[i]);
+    }
 
-printf("\n\nAVERAGE WAITING TIME : %f",wavg);
-printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
-return 0;
+  printf("\n\nAVERAGE WAITING TIME : %f",wavg);
+  printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
+  return 0;
 }
